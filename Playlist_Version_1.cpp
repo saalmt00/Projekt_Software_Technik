9d96b7f37b52353586b03ade2f38aabd0a4b5631
BuildTools <unconfigured@null.spigotmc.org>
2024-05-26 15:18:49 +0200

    Playlist_V1

--- Playlist_Version_1.cpp
+++ Playlist_Version_1.cpp
@@ -5,55 +5,51 @@
 
 #define N 69
 
-void Einlesefunktion(std::string point_Playlist[], int &point_Count, std::string Lieder){
+void Einlesefunktion(std::string *point_Playlist, int &Count){
 
+    std::string Lieder = "Lieder.txt";
+    
     std::ifstream input(Lieder);
-    int i = point_Count;
 
+    //std::string Playlist;
+
+    int i = 0;
+
+    
     while(std::getline(input, point_Playlist[i])){
-        //std::cout << point_Playlist[i] << std:: endl;
+        std::cout << point_Playlist[i] << std:: endl;
+       
         i++;
-        point_Count = i;
-        //printf("%d \n",point_Count);
+
+        Count = i;
+
+        printf("%d \n",Count);
+
     }
 
-    //printf("%d \n",point_Count);
+    printf("%d \n",Count);
+
+    printf("hello");
+
+
     input.close();
 
 }
 
-void Playlist_auflisten(std::string point_Playlist[], int &point_Count){
-
-    printf("             Track Name              Artist Name                                                 Genres              Time\n\n");
-
-    for(int i=0; i<point_Count; i++){
-        if(i<9){
-            printf("Lied Nr. %d:  ", i+1);
-            std::cout << point_Playlist[i] << std::endl;
-        }
-
-        else{
-            printf("Lied Nr. %d: ", i+1);
-            std::cout << point_Playlist[i] << std::endl;
-        }
-
-    }
-
-    printf("\n\n");
-
-}
 
 int main(){
 
     std::string Playlist[N];
 
-    int Count = 0;
+    int Count;
 
-    Einlesefunktion(Playlist, Count, "Playlist_Spotify.txt");
+    Einlesefunktion(Playlist, Count);
 
-    //printf("%d \n",Count);
+    printf("%d \n",Count);
 
-    Playlist_auflisten(Playlist, Count);
+    for(int i=0; i<Count; i++){
+    std::cout << Playlist[i] << std:: endl;
+    }
 
     return 0;
 } 
